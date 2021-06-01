e9d938e8fd40fcc1050e9868a9862b9545cc1e7c
truong hung <truonghungvatvo@gmail.com>
2021-06-01 10:20:49 +0700

    refactor src

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import useStyles from './styles'
+
+export default function Home() {
+    const classes = useStyles();
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -9,28 +9,8 @@ import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import useStyles from './styles'
 
 export default function SignIn() {
   const classes = useStyles();
