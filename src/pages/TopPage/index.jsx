65f1996eaf56cd5e522532b7d0c70e30d2f99cbc
thangnk-0129 <nguyen.khac.thang@sun-asterisk.com>
2021-06-04 18:12:07 +0700

    Add top page

--- src/pages/TopPage/index.jsx
+++ src/pages/TopPage/index.jsx
@@ -1,11 +1,33 @@
 import {Link} from 'react-router-dom'
+import './top-page.css'
 
 const TopPage = () => {
     return (
-        <div>
-            <h2>Welcom to My team app</h2>
-            <div><Link to='/sign-in'><button>Sign in</button></Link></div>
-            <div><Link to='/sign-up'><button>Sign up</button></Link></div>
+        <div className='bg'>
+            <div>
+                <ul>
+                    <li className='nav__left'>
+                        <Link to='/' className='fa fa-instagram' />
+                    </li>
+                    <li className='nav__right'>
+                        <Link to='/sign-in'>Login</Link>
+                    </li>
+                    <li className='nav__right'>
+                        <Link to='/sign-up'>Register</Link>
+                    </li>
+                    <li className='nav__right'>
+                        <Link to=''>About Us</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className='main-content'>
+                <h1 className='content__title1' style={{fontSize: '50px'}}>INSTAGRAM</h1>
+                <h2 className='content__title2'>A Photo And Video Sharing Social Networking Service</h2>
+                <br />
+                <button className='btn'>
+                    <span><Link to='/sign-up' style={{textDecoration: 'none', color: 'white'}}>join with us </Link></span>
+                </button>
+            </div>
         </div>
     )
 }
