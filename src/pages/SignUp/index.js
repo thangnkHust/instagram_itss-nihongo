98dd62d717faaf6ecc131dc4744806bf181f3ab0
thangnk-0129 <nguyen.khac.thang@sun-asterisk.com>
2021-06-05 10:40:02 +0700

    Dessign Login and Register

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,36 +1,19 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import {Link} from 'react-router-dom'
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Grid,
+  Typography,
+  Container,
+} from '@material-ui/core';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import useStyles from './styles'
 
 export default function SignUp() {
   const classes = useStyles();
@@ -40,7 +23,7 @@ export default function SignUp() {
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+          <InstagramIcon className={classes.icon}/>
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign up
@@ -99,7 +82,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link to='/sign-in' variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
